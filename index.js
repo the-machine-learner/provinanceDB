2f49f4a8a335e3e25b66c779a0c0689bf713b43e
the-machine-learner <ritvikush@gmail.com>
2020-06-30 01:41:09 +0530

    second commit

--- index.js
+++ index.js
@@ -2,18 +2,41 @@ const process = require('process')
 const path = require('path')
 const express = require('express')
 const app = express()
+const mysql = require('mysql');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'me',
-    password : 'secret',
-    database : 'my_db'
+    user     : 'root',
+    password : 'password',
+    database : 'new_schema'
   });
    
-connection.connect();
+connection.connect(function(err) {
+    if (err) {
+      console.error('error connecting: ' + err.stack);
+      return;
+    }
+   
+    console.log('connected as id ' + connection.threadId);
+});
+
+app.get('/', (req, res) =>{
+    // connection.query('SELECT * FROM tab1', function (error, results, fields) {
+    //     if (error) throw error;
+    //     else{
+    //         console.log(results);
+    //         res.send(`The solution is: ${results[0]}`);
+    //     } 
+    //   }); 
+    res.send('Hello World!');
+    //   connection.end();
+});
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+
