dbdd4309be0b110348aba687dcefc33979c05bee
the-machine-learner <ritvikush@gmail.com>
2020-06-30 01:52:28 +0530

    third commit

--- index.js
+++ index.js
@@ -6,21 +6,21 @@ const mysql = require('mysql');
 
 const port = process.env.PORT || 3000;
 
-var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'password',
-    database : 'new_schema'
-  });
+// var connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'password',
+//     database : 'new_schema'
+//   });
    
-connection.connect(function(err) {
-    if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
-    }
+// connection.connect(function(err) {
+//     if (err) {
+//       console.error('error connecting: ' + err.stack);
+//       return;
+//     }
    
-    console.log('connected as id ' + connection.threadId);
-});
+//     console.log('connected as id ' + connection.threadId);
+// });
 
 app.get('/', (req, res) =>{
     // connection.query('SELECT * FROM tab1', function (error, results, fields) {
